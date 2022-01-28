b72506a9a44c51a289c859e2e9b08aa44fdfa8ee
Rahul <rahul.k@simformsolutions.com>
2022-01-28 17:17:51 +0530

    Feat: Added a  migration for book and author

--- db/migrate/20220128111705_add_author.rb
+++ db/migrate/20220128111705_add_author.rb
@@ -1,6 +1,6 @@
 class AddAuthor < ActiveRecord::Migration[7.0]
   def up
-    Author.create(first_name: "Rahul", lname: "Kumar", dob: "22/03/2000", email: "rahul@gmail.com")
+    Author.create(first_name: "Rahul", last_name: "Kumar", dob: "22/03/2000", email: "rahul@gmail.com")
   end
 
   def down
